9b1a93035fcd8c02dad0137c7b7a6269ae902941
JaiHindocha <44731225+JaiHindocha@users.noreply.github.com>
2020-10-24 02:14:16 +0400

    Add files via upload

---
+++ routes/product.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+const Product = require("../model/Product");
+
+router.get("/productAll",async (req, res) => {
+     Product.find({})
+         .then(oProduct => {
+             res.send(oProduct);
+         }).catch(err => {
+         res.status(500).send({
+             message: err.message || "Some error occurred while retrieving the product."
+         });
+     });
+});
+
+module.exports = router;
