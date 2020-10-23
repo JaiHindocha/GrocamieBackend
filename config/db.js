69dcc0180fdefddf11f840323927423a904d0715
Jai Hindocha <jai.hindocha@gmail.com>
2020-10-24 01:13:15 +0400

    initial commit

---
+++ config/db.js
@@ -0,0 +1,19 @@
+
+    const mongoose = require("mongoose");
+
+    // Replace this with your MONGOURI.
+    const MONGOURI = "mongodb+srv://jai:grocamie123@cluster0.f9wcc.mongodb.net/Grocamie?retryWrites=true&w=majority";
+    
+    const InitiateMongoServer = async () => {
+      try {
+        await mongoose.connect(MONGOURI, {
+          useNewUrlParser: true
+        });
+        console.log("Connected to DB !!");
+      } catch (e) {
+        console.log(e);
+        throw e;
+      }
+    };
+    
+    module.exports = InitiateMongoServer;   
