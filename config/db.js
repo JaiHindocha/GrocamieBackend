96915cce9180e3ca3b3ace774b3387a287f980bf
Deep's Laptop <deepdiwakar9@gmail.com>
2021-01-01 15:20:38 +0530

    updated MONGODB URI

--- config/db.js
+++ config/db.js
@@ -1,19 +1,18 @@
+const mongoose = require('mongoose');
 
-    const mongoose = require("mongoose");
+// Replace this with your MONGOURI.
+const MONGOURI = 'mongodb://localhost/grocamie';
 
-    // Replace this with your MONGOURI.
-    const MONGOURI = "mongodb+srv://jai:grocamie123@cluster0.f9wcc.mongodb.net/Grocamie?retryWrites=true&w=majority";
-    
-    const InitiateMongoServer = async () => {
-      try {
-        await mongoose.connect(MONGOURI, {
-          useNewUrlParser: true
-        });
-        console.log("Connected to DB !!");
-      } catch (e) {
-        console.log(e);
-        throw e;
-      }
-    };
-    
-    module.exports = InitiateMongoServer;   
+const InitiateMongoServer = async () => {
+  try {
+    await mongoose.connect(MONGOURI, {
+      useNewUrlParser: true,
+    });
+    console.log('Connected to DB !!');
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+};
+
+module.exports = InitiateMongoServer;
