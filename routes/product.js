483e8bc749ed54c97578572d72eb712e5a234cc8
Jai Hindocha <jai.hindocha@gmail.com>
2021-01-26 23:13:42 +0400

    productById

--- routes/product.js
+++ routes/product.js
@@ -21,6 +21,18 @@ router.get("/all",async (req, res) => {
      });
 });
 
+
+router.get("/productById",async (req, res) => {
+  Product.find({'_id':'req.body.id'})
+      .then(oProduct => {
+          res.send(oProduct);
+      }).catch(err => {
+      res.status(500).send({
+          message: err.message || "Some error occurred while retrieving the product."
+      });
+  });
+});
+
 // router.get("/get", async (req, res) => {
 //   // const errors = validationResult(req);
 //   // if (!errors.isEmpty()) {
