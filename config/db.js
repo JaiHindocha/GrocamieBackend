e95a2e3f5356b2881814844b861baa3acc10f46b
Jai Hindocha <jai.hindocha@gmail.com>
2021-01-27 00:17:59 +0400

    f

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@
     const mongoose = require("mongoose");
 
     // mongodb+srv://jai:grocamie123@cluster0.f9wcc.mongodb.net/Grocamie?retryWrites=true&w=majority 
-    // const MONGOURI = "mongodb://localhost/grocamie";
-    const MONGOURI = "mongodb+srv://jai:grocamie123@cluster0.f9wcc.mongodb.net/Grocamie?retryWrites=true&w=majority";
+    const MONGOURI = "mongodb://localhost/grocamie";
+    // const MONGOURI = "mongodb+srv://jai:grocamie123@cluster0.f9wcc.mongodb.net/Grocamie?retryWrites=true&w=majority";
 
 
     const InitiateMongoServer = async () => {
