62ab48235f3134a7dd7c8ad43fb74ab80834991b
Samar <>
2021-02-02 02:14:50 +0400

    changes

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const bodyParser = require("body-parser");
 const user = require("./routes/user"); //new addition
 const distributor = require("./routes/distributor"); //new addition
 const product = require("./routes/product"); //new addition
-const community = require("./routes/community"); //new addition
+const community = require("./routes/community");
+const categories = require("./routes/categories"); //new addition
 const cart = require("./routes/cart");
 const order = require("./routes/order");
 const cors = require('cors');
@@ -41,6 +42,7 @@ app.use("/product", product);
 app.use("/community", community);
 app.use("/cart", cart);
 app.use("/order", order);
+app.use("/categories", categories);
 
 
 app.post('/orders', (req, res) => {
